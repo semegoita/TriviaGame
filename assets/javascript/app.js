90f94ecf681b37f825fc6fb9c1a7a008fa753a63
semegoita <34927635+semegoita@users.noreply.github.com>
2018-03-25 21:28:29 -0700

    pseudocode added

---
+++ assets/javascript/app.js
@@ -0,0 +1,7 @@
+//total trivia pseudocode
+// get the start button to start the game
+//set the time remaining with countdown timer
+//get the questions and make the multiple answers with only one answer
+// add a done button to end the button and submiit
+// make the game end by it self if time is up
+// display results after timer is up or after done is hit.
